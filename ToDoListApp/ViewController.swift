30a48ce07a196dbbd24d7f21192ebd45df62d7a2
Aytaç Bulanık <aytacbulanik@hotmail.com>
2022-12-01 16:27:54 +0300

    tableview is working

--- ToDoListApp/ViewController.swift
+++ ToDoListApp/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var taskLabel : UILabel!
+    @IBOutlet var taskField : UITextField!
+    @IBOutlet var addButtonOut : UIButton!
+    @IBOutlet var tableView : UITableView!
+    
+    var taskArray : [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return taskArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Deneme"
+        return cell
+    }
+    @IBAction func addButtonPressed(_ sender : UIButton) {
+       
+    }
 
 }
 
